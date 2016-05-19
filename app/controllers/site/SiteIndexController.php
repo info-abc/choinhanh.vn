f2d08b57dbba5e31c054fe591b71ca41f620e5c9
dxnm <2711sq@gmail.com>
2016-05-19 08:10:38 +0700

    fix

--- app/controllers/site/SiteIndexController.php
+++ app/controllers/site/SiteIndexController.php
@@ -10,15 +10,16 @@ class SiteIndexController extends SiteController {
 	public function index()
 	{
 		//return View::make('maintance');
-		if(getDevice() == MOBILE) {
-			$path = public_path().FOLDER_HTML_CODE.'/index_mobile.html';
-	    	$text = file_get_contents($path);
-		} else {
-			$path = public_path().FOLDER_HTML_CODE.'/index_pc.html';
-	    	$text = file_get_contents($path);
-		}
-		return $text;
-		// return View::make('site.index');
+		// if(getDevice() == MOBILE) {
+		// 	$path = public_path().FOLDER_HTML_CODE.'/index_mobile.html';
+	 //    	$text = file_get_contents($path);
+		// } else {
+		// 	$path = public_path().FOLDER_HTML_CODE.'/index_pc.html';
+	 //    	$text = file_get_contents($path);
+		// }
+		// return $text;
+		$device = null;
+		return View::make('site.index')->with(compact('device'));
 	}
 
 	public function home()
