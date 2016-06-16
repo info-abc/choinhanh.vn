090425634725413b4bc05b569965069eb42ecbc8
si1991Van <sivannguyen1991@gmail.com>
2016-06-16 14:48:17 +0700

    fix

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'gamemini',
+			'database'  => 'testchoinhanh',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
