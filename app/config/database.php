e37835dce0c68092f9d8d0e232f9381eaa373274
dxnm <2711sq@gmail.com>
2016-06-17 09:58:31 +0700

    fix

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'testchoinhanh',
+			'database'  => 'gamemini',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
