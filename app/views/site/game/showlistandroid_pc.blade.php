2b45dc3e12fe0d3e34eef95658fa5c7022f33928
dxnm <2711sq@gmail.com>
2016-06-21 14:14:30 +0700

    fixx

--- app/views/site/game/showlistandroid_pc.blade.php
+++ app/views/site/game/showlistandroid_pc.blade.php
@@ -43,7 +43,35 @@
 {{-- quang cao --}}
 @include('site.common.ads', array('adPosition' => POSITION_GAMEANDROID))
 
-
+<div class="box">
+	<h3>Game Android mới nhất</h3>
+	<?php
+		$games = CommonGame::getListGame('android', 2);
+		$count = ceil(count($games->get())/PAGINATE_BOXGAME);
+	?>
+	<div class="swiper-container">
+		<div class="swiper-wrapper">
+			@for($i = 0; $i < $count ; $i ++)
+				<div class="swiper-slide boxgame">
+					<div class="row">
+					<?php
+						$listGame = $games->orderBy('id', 'desc')->take(PAGINATE_BOXGAME)->skip($i * PAGINATE_BOXGAME)->get();
+					?>
+						@foreach($listGame as $game)
+							@include('site.game.gameitem_cronjob', array('game' => $game, 'slug' => null, 'device' => 2))
+						@endforeach
+					</div>
+				</div>
+			@endfor
+		</div>
+		<div class="swiper-pagination"></div>
+		<div class="boxgame-pagination">
+			<a class="prev"><i class="fa fa-caret-left"></i> Trang trước</a>
+			<div class="boxgame-pagenumber"><span class="numberPage">1</span>/{{ $count }}</div>
+			<a class="next">Trang sau <i class="fa fa-caret-right"></i></a>
+		</div>
+	</div>
+</div>
 
 @include('site.game.scriptbox')
 
